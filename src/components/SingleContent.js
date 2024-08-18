75d226fd466057507eff9e8df8c00d7258abc62b
NICE-97 <pattanawongsangpan@gmail.com>
2024-08-18 16:43:18 +0700

    first commit

---
+++ src/components/SingleContent.js
@@ -0,0 +1,19 @@
+import { useState } from "react"
+import { FaPlus,FaMinus } from "react-icons/fa";
+
+const SingContent = ({title,description})=>{
+    const [showContent,setShowContent] = useState(false);
+    return(
+        <article className="content">
+            <header>
+                <h4>{title}</h4>
+                <button className="btn" onClick={()=>setShowContent(!showContent)}>
+                    {showContent ? <FaMinus/> : <FaPlus/>}
+                </button>
+            </header>
+            {showContent && <p>{description}</p>}
+        </article>
+    )
+}
+
+export default SingContent
